43b09c6fb5dbc1713bdaa23266d94afdaeed8083
Mariana <yo_sweet_mary@yahoo.com>
2024-11-11 08:21:19 +0000

    add .gitignore

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,19 @@
+//Nav hamburger selections
+ const burger = document.querySelector("#burger-menu");
+    const ul = document.querySelector("nav ul");
+    const nav = document.querySelector("nav")
+  
+  burger.addEventListener("click" , () => {
+    ul.classList.toggle("show");
+  });
+  
+  //close hamburger menu when a link is clicked
+  // Select nav link
+  const navLinks = document.querySelectorAll(".nav-link");
+  
+  navLinks.forEach((link) => {
+  link.addEventListener("click" , () => {
+    ul.classList.remove("show");
+  });
+  });
+  
